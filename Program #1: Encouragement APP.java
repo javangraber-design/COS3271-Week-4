1261babde87294e7f1485f3775c01055e7897c38
Javan Graber <jmgraber@students.unwsp.edu>
2026-09-25 15:40:28 -0500

    Update Program #1: Encouragement APP.java

--- Program #1: Encouragement APP.java
+++ Program #1: Encouragement APP.java
@@ -11,7 +11,7 @@ public class MyProject {
     	int feeling;
     	double age;
     	
-    	//Print the options
+    	//Display the options
     	System.out.println("Here is a list of the options for your current day:\n");
     	System.out.println("\t1: I'm having a really bad day and am feeling extremely sad.");
     	System.out.println("\t2: I'm not having a good day and I feel sad.");
@@ -32,28 +32,28 @@ public class MyProject {
         //Analyze the feelings and respond
         System.out.print("\n");
         if (feeling == 1) {
-        	//Analyze the age for the correct response
+        	//Analyze the age for the correct response for a very sad attitude
         	if (age <= 6) {System.out.println("Make sure to pray to Jesus about it. He will help you be happy.");}
         	else if (age <= 13) {System.out.println("Have you talked to God about your feelings? Praying almost always helps.");}
         	else {System.out.println("I'm so sorry that you're upset! I recommend praying to God about it and asking Him for peace.");}	
         }
         
         else if (feeling == 2) {
-        	//Analyze the age for the correct response
+        	//Analyze the age for the correct response for a sad attitude
         	if (age <=6) {System.out.println("I'm sorry that you're sad. Talk to God about it.");}
         	else if (age <= 13) {System.out.println("I'm sorry that you're upset. Make sure to tell God about any of your struggles.");}
         	else {System.out.println("I'm sorry that you are not feeling great today. You should pray to God about it and try to focus on the things you are thankful for.");}
         }
         		
         else if (feeling == 3) {
-        	//Analyze the age for the correct response
+        	//Analyze the age for the correct response for a neutral attitude
         	if (age <=6) {System.out.println("You should find something fun to play or someone fun to play with!");}
         	else if (age <= 13) {System.out.println("You should try to find something fun to do or pause to think about some happy things that you could do later or even now!");}
         	else {System.out.println("It seems like you're ready to find something to be happy about. Think about the ways your life has been blessed.");}
         }
         		
         else if (feeling == 4) {
-        	//Analyze the age for the correct response
+        	//Analyze the age for the correct response for a happy attitude
         	if (age <=6) {System.out.println("I'm so happy that you're happy! I hope your day stays great!");}
         	else if (age <= 13) {System.out.println("I'm feeling happy just knowing that you're happy! Try to keep this happiness all day long and share it with those around you!");}
         	else {System.out.println("I'm happy to hear that you're happy! You should try to share this happiness with others too!");}
@@ -61,7 +61,7 @@ public class MyProject {
         
 
         else if (feeling == 5) {
-        	//Analyze the age for the correct response
+        	//Analyze the age for the correct response for a very happy attitude
         	if (age <=6) {System.out.println("I'm so excited that you're happy! God loves a positive attitude!");}
         	else if (age <= 13) {System.out.println("I'm so glad today has been so great for you! God sure has blessed you!");}
         	else {System.out.println("I am so excited to hear that your day has been great! It sure seems like God has blessed you! Don't ever forget about the joy He brings!");}
